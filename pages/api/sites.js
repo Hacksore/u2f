74d25c6a6eb8faa4c9fcf8116884414e586340e6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-06-10 08:22:44 -0500

    Add cache header

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -5,6 +5,7 @@ export default async function handler(_, res) {
   const url = "https://2fa.directory/api/v3/u2f.json";
   const response = await got(url).json();
 
+  res.setHeader("Cache-Control", "s-maxage=86400");
   res.status(200).json(response)
 
 }
